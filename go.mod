8c03ab9eb909a62230ab06dceabf2f34d2707f5b
lisson <li.shangjin@icloud.com>
2019-12-25 14:54:00 +0800

    add namespace interface.

--- go.mod
+++ go.mod
@@ -10,6 +10,11 @@ require (
 	go.uber.org/zap v1.13.0
 	golang.org/x/crypto v0.0.0-20191219195013-becbf705a915 // indirect
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	k8s.io/api v0.17.0
 	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v0.0.0-20191016230210-14c42cd304d9
 	k8s.io/klog v1.0.0
+	k8s.io/utils v0.0.0-20191218082557-f07c713de883 // indirect
 )
---
+++ pkg/store/store.go
@@ -0,0 +1,31 @@
+package store
+
+import (
+	"context"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/tools/cache"
+)
+
+type Key struct {
+	Namespace  string
+	APIVersion string
+	Kind       string
+	Name       string
+	Selector   *labels.Set
+}
+
+// Store stores Kubernetes objects.
+type Store interface {
+	List(ctx context.Context, key Key) (list *unstructured.UnstructuredList, loading bool, err error)
+	Get(ctx context.Context, key Key) (object *unstructured.Unstructured, found bool, err error)
+	Delete(ctx context.Context, key Key) error
+	Watch(ctx context.Context, key Key, handler cache.ResourceEventHandler) error
+	Unwatch(ctx context.Context, groupVersionKinds ...schema.GroupVersionKind) error
+	//UpdateClusterClient(ctx context.Context, client cluster.ClientInterface) error
+	RegisterOnUpdate(fn UpdateFn)
+	IsLoading(ctx context.Context, key Key) bool
+}
+
+type UpdateFn func(store Store)
