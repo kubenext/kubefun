3c6f6fffcc2842360a58513200530066515e301c
lisson <li.shangjin@icloud.com>
2019-12-26 00:07:16 +0800

    add loading for internal.

---
+++ internal/loading/loading.go
@@ -0,0 +1,12 @@
+package loading
+
+import (
+	"context"
+	"github.com/kubenext/kubefun/pkg/store"
+)
+
+// IsObjectLoading returns true if objects described by a key are loading.
+func IsObjectLoading(ctx context.Context, namespace string, key store.Key, objectStore store.Store) bool {
+	key.Namespace = namespace
+	return objectStore.IsLoading(ctx, key)
+}
